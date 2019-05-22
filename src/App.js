b45e6d0e14d09cf5b18cd5be453ac4ba33f74cb4
hectorscreativeworld <hectorscreativeworld@gmail.com>
2019-05-22 15:17:26 -0400

    ColorSlider

--- src/App.js
+++ src/App.js
@@ -1,9 +1,64 @@
 import React, { Component } from 'react'
-import HelloWorld from './components/HelloWorld'
 
 class App extends Component {
+  HueSlider = event => {
+    console.log(event.target.value)
+  }
+  SatSlider = event => {
+    console.log(event.target.value)
+  }
+  LightSlider = event => {
+    console.log(event.target.value)
+  }
   render() {
-    return <HelloWorld />
+    return (
+      <>
+        <h1 class="MainTitle">Cool Color Picker</h1>
+        <main class="MainContainer">
+          <h2>Hue</h2>
+
+          <div>
+            <input
+              className="HueSlider"
+              type="range"
+              id="start"
+              name="volume"
+              min="0"
+              max="255"
+              onChange={this.HueSlider}
+            />
+            <label for="volume" />
+          </div>
+          <h2>Saturation</h2>
+          <div>
+            <input
+              className="HueSlider"
+              type="range"
+              id="start"
+              name="volume"
+              min="0"
+              max="255"
+              onChange={this.SatSlider}
+            />
+            <label for="cowbell" />
+          </div>
+
+          <h2>Lightness</h2>
+          <div>
+            <input
+              className="HueSlider"
+              type="range"
+              id="start"
+              name="volume"
+              min="0"
+              max="100"
+              onChange={this.LightSlider}
+            />
+            <label for="cowbell" />
+          </div>
+        </main>
+      </>
+    )
   }
 }
 
